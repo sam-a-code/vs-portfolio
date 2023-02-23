66d4d27670b05fc60f08d597e526fca722fad512
Sam Avery <sam.avery1@gmail.com>
2023-02-23 12:22:45 -0600

    adding logos for contact

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,39 @@
 function Footer() {
     return (
         <>
-        <h1>footer</h1>
+        {/* <div className="color">
+            <img src="https://img.icons8.com/color/48/null/vimeo.png"/>
+            <img src="https://img.icons8.com/color/48/null/imdb.png"/>
+            <img src="https://img.icons8.com/color/48/null/linkedin-circled--v1.png"/>
+        </div> */}
+        {/* <div className="dark-background-black">
+            <img src="https://img.icons8.com/external-tanah-basah-glyph-tanah-basah/48/null/external-vimeo-social-media-tanah-basah-glyph-tanah-basah.png"/>
+            <img src="https://img.icons8.com/ios-filled/50/null/imdb.png"/>
+            <img src="https://img.icons8.com/ios-filled/50/null/linkedin-circled--v1.png"/>
+        </div> */}
+        <div className="footer-icons">
+            <a href='https://www.linkedin.com/in/victorialynnmoreno/' target='_blank'>
+                <img src="https://img.icons8.com/ios-filled/50/FFFFFF/linkedin.png"/>
+            </a>
+            <a href="https://www.imdb.com/name/nm5640145/" target="_blank">
+                <img src="https://img.icons8.com/ios-filled/50/FFFFFF/imdb.png"/>
+            </a>
+            <a>
+                <img src="https://img.icons8.com/ios-filled/50/FFFFFF/vimeo.png"/>
+            </a>
+        </div>
+        {/* <div className="outlines-black">
+            <img src="https://img.icons8.com/ios/50/null/vimeo.png"/>
+            <img src="https://img.icons8.com/ios/50/null/imdb.png"/>
+            <img src="https://img.icons8.com/ios/50/null/linkedin-2--v1.png"/>
+            <img src="https://img.icons8.com/ios/50/null/linkedin-circled--v1.png"/>
+        </div> */}
+        {/* <div className="outlines-white">
+            <img src="https://img.icons8.com/ios/50/FFFFFF/linkedin-circled--v1.png"/>
+            <img src="https://img.icons8.com/ios/50/FFFFFF/linkedin.png"/>
+            <img src="https://img.icons8.com/ios/50/FFFFFF/imdb.png"/>
+            <img src="https://img.icons8.com/ios/50/FFFFFF/vimeo.png"/>
+        </div> */}
         </>
 
     );
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,9 @@ import Bio from './Bio'
 function Home() {
     return (
         <div className='home'>
-            <h1>Victoria Moreno</h1>
-            <h2>VFX Producer / Editor</h2>
+        <img src='https://i.imgur.com/nWf8zqc.png' className='home-image'/>
+            <h1 className='name'>Victoria Moreno</h1>
+            <h2 className='title'>VFX Producer / Editor</h2>
         </div>
 
     );
